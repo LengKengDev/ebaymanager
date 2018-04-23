e88493eb6a7783ec10457c33f9f0807fa5202f7b
ThienLV <ohmygodvt95@gmail.com>
2018-04-24 03:03:27 +0700

    table ajax

--- resources/views/orders/_status.blade.php
+++ resources/views/orders/_status.blade.php
@@ -1,9 +1,6 @@
 {{ucwords(str_replace("_", " ", $status))}}
 @if(strlen($tracking) > 0)
-    <a href="#" class="btn btn-success btn-xs pull-right" onclick="event.preventDefault();if(confirm('Are you sure you want to update status order?')){
-            document.getElementById('order-status-form-{{$id}}').submit();}">
+    <button href="{{url()->route('api.tracking.show', ['order' => $id], false)}}" class="btn btn-success btn-status btn-xs pull-right">
         <i class="fa fa-fw fa-refresh"></i>
-    </a>
-    <form id="order-status-form-{{$id}}" action="{{ url()->route('api.tracking.show', ['order' => $id]) }}" method="GET" style="display: none;">
-    </form>
+    </button>
 @endif
